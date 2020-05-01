2e044d9f8b1408cdbf2aae251be3793c9038dabd
吴环宇 <whyaiwxf@gmail.com>
2020-05-01 15:14:58 +0800

    add routing socket server

---
+++ routing/server.py
@@ -0,0 +1,65 @@
+import socket
+import sys
+import json
+from json import JSONDecodeError
+import threading
+import socketserver
+from utils.common_utils import is_digit, info,debug
+from sockets.server import Server
+import random
+import networkx as nx
+from itertools import islice
+
+K=3
+g:nx.Graph=nx.grid_graph([3,3])
+g=nx.relabel_nodes(g,lambda x:x[0]*3+x[1])
+
+def k_shortest_paths(G, source, target, k, weight=None):
+	return list(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
+
+ksps=[]
+for i in range(g.number_of_nodes()):
+	for j in range(g.number_of_nodes()):
+		if i==j:continue
+		ksps.append(k_shortest_paths(g,i,j,K))
+
+debug("ksp calculated")
+
+def check(content:str):
+	try:
+		obj=json.loads(content)
+	except JSONDecodeError:
+		return -1
+	if "volumes" not in list(obj.keys()):
+		return -1
+	volumes=obj["volumes"]
+	if len(volumes)!=K*(K-1)*2:
+		return -1
+
+	return volumes
+
+class DumbHandler(socketserver.BaseRequestHandler):
+	def handle(self) -> None:
+		req_str=str(self.request.recv(1024),"utf-8")
+		vols=check(req_str)
+		if vols==-1:
+			return
+		l=len(vols)
+		res=[]
+		for i in range(l):
+			randidx=random.randint(0,K-1)
+			res.append(ksps[i][randidx])
+		res={"res":res}
+		self.request.sendall(bytes(json.dumps(res),"utf-8"))
+
+
+if __name__ == '__main__':
+    port=10000
+    server=Server(10000,DumbHandler)
+    server.start()
+
+
+
+
+
+
--- test/socket_test.py
+++ test/socket_test.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
 	with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
 		sock.connect(("localhost",10000))
-		content={"stats":[1,2,3,4,5,6,7,8]}
+		content={"volumes":[1,2,3,4,5,6,7,8,9,10,11,12]}
 		sock.sendall(bytes(json.dumps(content),"utf-8"))
 		resp=str(sock.recv(2048),"utf-8")
 		print(resp)
