a25db7af4998b7edcdbc1569b3805fbc2aa67557
吴环宇 <whyaiwxf@gmail.com>
2020-05-03 12:54:47 +0800

    add ditg module

---
+++ traffic/ditg/src/ITGManager/ITGManager.h
@@ -0,0 +1,64 @@
+/*
+ *   Component of the D-ITG 2.8.1 (r1023) platform (http://traffic.comics.unina.it/software/ITG)
+ *
+ *   Copyright     : (C) 2004-2013 by Alessio Botta, Walter de Donato, Alberto Dainotti,
+ *                                      Stefano Avallone, Antonio Pescape' (PI)
+ *                                      COMICS (COMputer for Interaction and CommunicationS) Group
+ *                                      Department of Electrical Engineering and Information Technologies
+ *                                      University of Napoli "Federico II".
+ *   email         : a.botta@unina.it, walter.dedonato@unina.it, alberto@unina.it,
+ *                   stavallo@unina.it, pescape@unina.it
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 		     
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *				     
+ *   For commercial use please refer to D-ITG Professional.
+ */
+
+ 
+
+
+
+
+
+
+
+#include "../libITG/ITGapi.h"
+#include <stdio.h>
+#include <string.h>
+
+#ifdef UNIX
+#include <pthread.h>
+#endif
+
+#include "../common/thread.h"
+
+#ifdef WIN32
+extern HANDLE mutexLog;
+#endif
+
+
+
+
+
+void* waitStopKey(void* s);
+
+void terminate(int sig);
+
+void read_ip_files(std::string& fn);
+void append_params(std::string& command,std::string key,std::string value);
+
+//void append_params(std::string& command,std::string& key);
+
+void send_socket(char* ip,int port,char* msg);
+
