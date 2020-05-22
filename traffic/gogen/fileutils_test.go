55c6c4588ce7073f03d7c3e563a7a8e76f7321a7
吴环宇 <whyaiwxf@gmail.com>
2020-05-22 18:55:24 +0800

    add:golisten

---
+++ traffic/gogen/fileutils_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFileExist(t *testing.T) {
+	fn:="/tmp/pkts/pkts.pkts"
+	if FileExist(fn){
+		fmt.Println("yes")
+	}else{
+		t.Fatal("wrong answer")
+	}
+
+	fn="/tmp/pkts/pktss"
+	if FileExist(fn){
+		t.Fatal("wrong answer")
+	}else{
+		fmt.Println("yes")
+	}
+}
+
+func TestDirExists(t *testing.T) {
+	dirFn:="/tmp/pkts"
+	if !DirExists(dirFn){
+		t.Fatal("Wrong")
+	}
+	dirFn="/tmp/hellodfd"
+	if DirExists(dirFn){
+		t.Fatal("wrong")
+	}
+}
+func TestReadLines(t *testing.T) {
+	lines,err:=ReadLines("/tmp/pkts/pkts.pkts")
+	if err!=nil{
+		t.Fatal("Wrong!")
+	}else{
+		fmt.Println(len(lines))
+		fmt.Println(lines[0])
+	}
+}
---
+++ traffic/gogen/netutils_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+func TestNetUtils_GenerateIP(t *testing.T)  {
+	ids:=[]int{1,2,3,4,5,6,7,254,255,256,10000}
+	for _,id:=range ids{
+		ip,err:=GenerateIP(id)
+		if err!=nil{
+			log.Fatalln(err)
+		}
+		fmt.Println(ip)
+	}
+}
+
+func TestNetUtils_GenerateMAC(t *testing.T)  {
+
+	ids:=[]int{0,1,2,3,4,5,6,7,10,256}
+	for _,id:=range ids{
+		mac,err:=GenerateMAC(id)
+		if err!=nil{
+			log.Fatalln(err)
+		}
+		fmt.Println(mac)
+	}
+}
