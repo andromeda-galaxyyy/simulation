e1233118f378bf368e14e3c3bb0c3c9913b3b74a
stack <whyaiwxf@gmail.com>
2020-09-07 09:45:17 +0800

    add:bin directory to gogen

--- traffic/gogen/build.sh
+++ traffic/gogen/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(cd ./gen && go build -o /tmp/gen)
-(cd ./golisten && go build -o /tmp/listen)
+(cd ./gen && go build -o ../bin/gogen && cp ../bin/gogen /tmp/gogen)
+(cd ./golisten && go build -o ../bin/golisten && cp ../bin/golisten /tmp/gogen)
