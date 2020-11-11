9dc3d0f7de718c8ab647dcd9773570f2f11190ac
吴环宇 <whyaiwxf@gmail.com>
2020-11-11 14:37:55 +0800

    add:flow counter api

--- traffic/gogen/server/router.go
+++ traffic/gogen/server/router.go
@@ -251,5 +251,16 @@ func GetLossBetween(c *gin.Context)  {
 }
 
 func GetFlowCounter(c *gin.Context)  {
+	stats,err:=counterReader.Read()
+	if err!=nil{
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg":"Internel Error",
+		})
+		return
+	}
 
+	c.JSON(http.StatusOK, gin.H{
+		"res":stats,
+	})
+	return
 }
